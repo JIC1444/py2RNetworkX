ceed1b99c11b245780968e7ed84226407ce468f4
JIC1444 <158279190+JIC1444@users.noreply.github.com>
2024-04-21 13:16:17 +0100

    Update createNXGraph.R

--- R/createNXGraph.R
+++ R/createNXGraph.R
@@ -1,11 +1,11 @@
 library(reticulate)
 createGraph <- function(df_location, phyla, about_location,
-                            y_axis, y_axis_label, save_location, gtype) {
+                            y_axis, y_axis_label, save_location) {
 
   py_run_file("Python/NXFunctions.py")
 
   py$createGraph(df_location, phyla, about_location,
-                   y_axis, y_axis_label, save_location, gtype)
+                   y_axis, y_axis_label, save_location)
 }
 
 
