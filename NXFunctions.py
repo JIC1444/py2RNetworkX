c937bf7856846c938f5799926320ac58072ee403
JIC1444 <158279190+JIC1444@users.noreply.github.com>
2024-04-21 13:06:04 +0100

    Update NXFunctions.py
    Changed Py Function name to match R function name

--- NXFunctions.py
+++ NXFunctions.py
@@ -5,7 +5,7 @@ import networkx as nx
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
-def CreateNXGraph(df_location, phyla, about_location, save_location, gtype):
+def createGraph(df_location, phyla, about_location, save_location, gtype):
   df = pd.read_csv(df_location, sep=" ", names = None) 
   df = df.reset_index()
   df = df.rename(columns={"index": "Location"})
