43d0f8487d3d8c44d6795ea50fd37e6808722c79
JIC1444 <158279190+JIC1444@users.noreply.github.com>
2024-04-21 13:19:17 +0100

    Update createNXGraph.R
    - Removed y_axis and y_axis_label parameters for now.

--- R/createNXGraph.R
+++ R/createNXGraph.R
@@ -1,11 +1,9 @@
 library(reticulate)
-createGraph <- function(df_location, phyla, about_location,
-                            y_axis, y_axis_label, save_location) {
+createGraph <- function(df_location, phyla, about_location, save_location) {
 
   py_run_file("Python/NXFunctions.py")
 
-  py$createGraph(df_location, phyla, about_location,
-                   y_axis, y_axis_label, save_location)
+  py$createGraph(df_location, phyla, about_location, save_location)
 }
 
 
