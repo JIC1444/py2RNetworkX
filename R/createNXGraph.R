23574ce8c6bef6b554fbd0bbd0c83e45d304513b
JIC1444 <158279190+JIC1444@users.noreply.github.com>
2024-04-21 11:34:06 +0100

    Update createNXGraph.R
    createGraph function (R) now looks for the python script NXFuntions.py in a Python folder in the RStudio Project

--- R/createNXGraph.R
+++ R/createNXGraph.R
@@ -2,7 +2,7 @@ library(reticulate)
 createGraph <- function(df_location, phyla, about_location,
                             y_axis, y_axis_label, save_location, gtype) {
 
-  py_run_file("NXFunctions.py")
+  py_run_file("Python/NXFunctions.py")
 
   py$CreateNXGraph(df_location, phyla, about_location,
                    y_axis, y_axis_label, save_location, gtype)
